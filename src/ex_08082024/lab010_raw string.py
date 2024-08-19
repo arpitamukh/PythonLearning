aca6618f8c4a5348d808b49044abd296f57f5336
ArpitaMukherjee <arpitamukherjee@rssoftware.co.in>
2024-08-19 12:29:37 +0530

    lab010

---
+++ src/ex_08082024/lab010_raw string.py
@@ -0,0 +1,9 @@
+#raw strings
+
+regular_string = "This is a newline character: \n and a tab: \t"
+print(regular_string)
+
+raw_string = r"This is a newline character: \n and a tab: \t"
+print(raw_string)
+
+print(r'printing of folder path :C:\python\class\n')
