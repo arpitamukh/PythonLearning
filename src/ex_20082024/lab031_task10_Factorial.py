0829f86cbda54ac2e3ee6059ed4673a97b8bb8e1
ArpitaMukherjee <arpitamukherjee@rssoftware.co.in>
2024-08-27 16:37:10 +0530

    assignment solution

--- src/ex_20082024/lab031_task10_Factorial.py
+++ src/ex_20082024/lab031_task10_Factorial.py
@@ -1,5 +1,10 @@
+import math
+
 num = int(input("enter the number:"))
-f = 1
-for i in range(1, num + 1):
-    f = f * i
-    print(f)
+# f = 1
+# for i in range(1, num + 1):
+#     f = f * i
+#     print(f)
+
+f = math.factorial(num)
+print(f)
